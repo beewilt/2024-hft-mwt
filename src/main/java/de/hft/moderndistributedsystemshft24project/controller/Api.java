d56c0dec00acf56883b3c6b7e08f10451d9c9cd4
BeekeWiltfang <beeke@wiltfang.de>
2024-12-15 21:48:30 +0100

    implement methods and delete ApiService

--- src/main/java/de/hft/moderndistributedsystemshft24project/controller/Api.java
+++ src/main/java/de/hft/moderndistributedsystemshft24project/controller/Api.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -22,11 +23,23 @@ public class Api implements ApiApi {
         this.apiRepository = apiRepository;
     }
 
-
     @Override
     public ResponseEntity<ShoppingItem> addItem(ShoppingItem shoppingItem) {
-        ShoppingItem sh = apiRepository.save(shoppingItem);
-        return new ResponseEntity<ShoppingItem>(sh, HttpStatus.CREATED);
+        return apiRepository.findByNameIgnoreCase(shoppingItem.getName())
+                .map(item -> {
+                    item.setAmount(item.getAmount() + shoppingItem.getAmount());
+                    return new ResponseEntity<>(apiRepository.save(item), HttpStatus.OK);
+                })
+                .orElseGet(() -> new ResponseEntity<>(apiRepository.save(shoppingItem), HttpStatus.CREATED));
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteItem(String name) {
+        return apiRepository.findByNameIgnoreCase(name)
+                .map(itemToDelete -> {
+                    apiRepository.delete(itemToDelete);
+                    return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+                }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @Override
@@ -34,7 +47,20 @@ public class Api implements ApiApi {
         return new ResponseEntity<>(apiRepository.findAll(), HttpStatus.OK);
     }
 
+    @Override
+    public ResponseEntity<ShoppingItem> getItemByName(String name) {
+        return apiRepository.findByNameIgnoreCase(name)
+                .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-
-
+    @Override
+    public ResponseEntity<ShoppingItem> updateItem(String name, ShoppingItem shoppingItem) {
+        return apiRepository.findByNameIgnoreCase(name)
+                .map(itemToUpdate -> {
+                    itemToUpdate.setName(shoppingItem.getName());
+                    itemToUpdate.setAmount(shoppingItem.getAmount());
+                    return new ResponseEntity<>(apiRepository.save(itemToUpdate), HttpStatus.OK);
+                }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
